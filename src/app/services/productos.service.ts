fd2ff761d93a69f8fa1da347164232a3dd0b4170
Carlos Rodriguez <carlos.amodri@gmail.com>
2020-04-28 11:04:43 -0300

    Filtrado del search

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ProductoInterface } from '../interfaces/prductoInterface';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
   productos : ProductoInterface[] = [];
+  productosFiltrados : ProductoInterface[] = [];
   cargando = true;
 
   constructor( private http : HttpClient) {
@@ -14,12 +16,47 @@ export class ProductosService {
    }
 
   private cargarProductos(){
+    return new Promise( (resolve, rejects) =>{
       this.http.get('https://angular-practica-6bad4.firebaseio.com/productos_idx.json')
-        .subscribe( ( resp : ProductoInterface[] ) => {
+      .subscribe( ( resp : ProductoInterface[] ) => {
+         this.productos = resp;
+         this.cargando = false;
+         resolve();
+      });
+    });
 
-           console.log(resp);
-           this.productos = resp;
-           this.cargando = false;
-        });
+ 
   }
+
+  getProducto ( id : string){
+    return this.http.get(`https://angular-practica-6bad4.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto (termino : string) {
+
+    if (this.productos.length == 0){
+      this.cargarProductos().then( ()=>{
+        this.filtrarProductos(termino);
+      });
+    }else{
+        this.filtrarProductos(termino);
+    }
+
+  }
+
+  private filtrarProductos(termino : string) {
+    console.log(this.productos);
+
+    this.productosFiltrados = [];
+    // termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+
+      let titulow = prod.titulo.toLowerCase();
+        if( prod.categoria.indexOf(termino) >= 0 || titulow.indexOf(termino) >= 0){
+          this.productosFiltrados.push(prod);
+        }
+    }); 
+  }
+
 }
